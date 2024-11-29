c38cad0fe964af890706b13d0870705751d1a3da
EnricoGenovese <enricogeno@libero.it>
2024-11-29 14:28:20 +0100

    add people.js; import functions as a const each w/ the require() method;

---
+++ people.js
@@ -0,0 +1,11 @@
+/*3 - people.js
+1. Importa la tua funzione da names.js
+2. Importa la tua funzione da hobbies.js
+3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+*/
+
+// 1 - Importing names function w/ require method (as opposed to the more modern 'import')
+const names = require("./names");
+
+// 2 - Importing hobbies function w/ require method (as opposed to the more modern 'import')
+const hobbies = require("./hobbies");
